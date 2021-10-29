debfdddfd4caa9db8d62e2e4acb447b3cb3fbf25
mva13 <markvallis1987@Gmail.com>
2021-10-29 11:40:09 +0100

    Step 5 of scala-js tutorial complete

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,39 @@
-
-import org.scalajs.dom.{Node, document}
+import org.scalajs.dom.{Event, MouseEvent, Node, document}
 
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 object Main extends App {
 
+  document.addEventListener("DOMContentLoaded", { _: Event =>
+    setupUI()
+  })
+
+  def setupUI(): Unit = {
+    appendParagraph(document.body, "Hi there.")
+    appendButton(document.body, "click me")
+  }
+
   def appendParagraph(targetNode: Node, text: String): Unit = {
-    val paragraphNode = document.createElement("p")
-    paragraphNode.textContent = text
-    targetNode.appendChild(paragraphNode)
+    val paragraph = document.createElement("p")
+    paragraph.textContent = text
+    targetNode.appendChild(paragraph)
+  }
+
+  def appendButton(targetNode: Node, buttonText: String): Unit = {
+    val button = document.createElement("button")
+    button.textContent = buttonText
+    button.addEventListener(
+      "click",
+      { _: MouseEvent =>
+        addCLickedMessage()
+      }
+    )
+    targetNode.appendChild(button)
   }
 
   @JSExportTopLevel("addClickedMessage")
-  def addCLickedMessage(): Unit = {
+  def addCLickedMessage(): Unit =
     appendParagraph(document.body, "You clicked the button.")
-  }
 
-  appendParagraph(document.body, "Hi there.")
+
 }
